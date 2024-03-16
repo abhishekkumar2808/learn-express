f71e5dd675ebe375bfe684fb87242d02fe655807
abhishekkumar <kumar.abhishe@northeastern.edu>
2024-03-15 23:37:38 -0400

    done with 03-12-2024

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,34 @@
 const express = require('express')
 const router = express.Router();
+
+
+
+router.get('/read/usernames', (req, res) => {
+  console.log("get")
+  let usernames = req.users.map(function(user) {
+    return {id: user.id, username: user.username};
+  });
+  res.send(usernames);
+});
+
+
+const getUserByUsername = function (req, res) {
+  console.log("getUserByUsername");
+  const username = req.params.name; // Get the username from request parameters
+  const user = req.users.find(user => user.username === username); // Find user by username
+  console.log(user);
+  if (user) {
+    req.userEmail = user.email; // Add user's email to request object
+    return res.status(200).json(
+      [{
+        email: user.email
+      }]
+    )
+  } else {
+    return res.status(404).json({
+      error: { message: 'User not found', status: 404 }
+    });
+  }
+};
+
+router.get('/read/username/:name', getUserByUsername);
--- quiz/server.js
+++ quiz/server.js
@@ -5,12 +5,23 @@ const app = express();
 var cors = require('cors');
 const port = 8000;
 
+
+
+app.use(
+  cors({origin: 'http://localhost:3000'})
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 let users;
+
 fs.readFile(path.resolve(__dirname, '../data/users.json'), function(err, data) {
   console.log('reading file ... ');
   if(err) throw err;
   users = JSON.parse(data);
 })
+
 
 const addMsgToRequest = function (req, res, next) {
   if(users) {
@@ -25,21 +36,20 @@ const addMsgToRequest = function (req, res, next) {
   
 }
 
-app.use(
-  cors({origin: 'http://localhost:3000'})
-);
-app.use('/read/usernames', addMsgToRequest);
 
-app.get('/read/usernames', (req, res) => {
-  console.log("get")
-  let usernames = req.users.map(function(user) {
-    return {id: user.id, username: user.username};
-  });
-  res.send(usernames);
-});
+app.use(addMsgToRequest);
+ 
+app.use('/read', readUsers)
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.get('/read/usernames', (req, res) => {
+//   console.log("get")
+//   let usernames = req.users.map(function(user) {
+//     return {id: user.id, username: user.username};
+//   });
+//   res.send(usernames);
+// });
+
+
 app.use('/write/adduser', addMsgToRequest);
 
 app.post('/write/adduser', (req, res) => {
@@ -59,27 +69,27 @@ app.get('read/username/:name', (req, res)=>{
   res.send({"error":name})
 })
 
-app.use('/read/username/:name', addMsgToRequest);
-const getUserByUsername = function (req, res) {
-  console.log("getUserByUsername");
-  const username = req.params.name; // Get the username from request parameters
-  const user = req.users.find(user => user.username === username); // Find user by username
-  console.log(user);
-  if (user) {
-    req.userEmail = user.email; // Add user's email to request object
-    return res.status(200).json(
-      [{
-        email: user.email
-      }]
-    )
-  } else {
-    return res.status(404).json({
-      error: { message: 'User not found', status: 404 }
-    });
-  }
-};
+// app.use('/read/username/:name', addMsgToRequest);
+// const getUserByUsername = function (req, res) {
+//   console.log("getUserByUsername");
+//   const username = req.params.name; // Get the username from request parameters
+//   const user = req.users.find(user => user.username === username); // Find user by username
+//   console.log(user);
+//   if (user) {
+//     req.userEmail = user.email; // Add user's email to request object
+//     return res.status(200).json(
+//       [{
+//         email: user.email
+//       }]
+//     )
+//   } else {
+//     return res.status(404).json({
+//       error: { message: 'User not found', status: 404 }
+//     });
+//   }
+// };
 
-app.get('/read/username/:name', getUserByUsername);
+// app.get('/read/username/:name', getUserByUsername);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
